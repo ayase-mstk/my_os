d6ef61d16fd14169c5475d4baacedf335d19b033
mahayase <mahayase@minios.myguest.virtualbox.org>
2023-08-31 01:57:43 +0900

    :zap:[add] alloc pages

--- kernel.c
+++ kernel.c
@@ -2,6 +2,19 @@
 #include "common.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) { // can be memory leak
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
 
 void handle_trap(struct trap_frame *f) {
     uint32_t scause = READ_CSR(scause);
@@ -115,8 +128,15 @@ void putchar(char ch) {
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1); 
+
+    PANIC("booted!");
+    
     WRITE_CSR(stvec, (uint32_t) kernel_entry);
-    __asm__ __volatile__("unimp"); // 無効な命令
 
     for (;;) {
         __asm__ __volatile__("wfi");
